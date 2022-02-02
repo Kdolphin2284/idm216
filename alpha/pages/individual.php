385baf3737b268832b75ceb8a3c4d776aad0ce26
Kdolphin2284 <71986227+Kdolphin2284@users.noreply.github.com>
2022-02-02 17:10:37 -0500

    CSS and Individual Page Update
    Added padding.

--- alpha/pages/individual.php
+++ alpha/pages/individual.php
@@ -41,118 +41,121 @@ if ($result-> num_rows > 0) {
         <link rel="stylesheet" href="../css/normalize.css">
     </head>
     <body>
-        <a href="../index.php" class="home_button">Back</a>
-<div id="menu">
-    <?php
-    // Category
-    echo "<h1>$hs_category</h1>";
-    ?>
+        <div id="individual_options">
+            <a href="../index.php" class="home_button">Back</a>
+            <div id="menu">
+                <?php
+                // Category
+                echo "<h1>$hs_category</h1>";
+                ?>
 
-    <?php
-    // Base
-    if (sizeof($hs_base) > 1) {
-    ?>
-    <div class="base">
-        <?php
-        for($i = 0; $i<sizeof($hs_base); $i++){
-            $base = $hs_base[$i];
-            echo "<button class='food_choice'>$base</button>";
-        }
-        ?>
-    </div>
-    <?php
-    } else {
-        echo '';
-    }
-    ?>
+                <?php
+                // Base
+                if (sizeof($hs_base) > 1) {
+                ?>
+                <div class="base">
+                    <?php
+                    for($i = 0; $i<sizeof($hs_base); $i++){
+                        $base = $hs_base[$i];
+                        echo "<button class='food_choice'>$base</button>";
+                    }
+                    ?>
+                </div>
+                <?php
+                } else {
+                    echo '';
+                }
+                ?>
 
-    <?php
-    // Protein
-    if (sizeof($hs_protein) > 1) {
-    ?>
-    <div class="protein">
-        <?php
-        for($i = 0; $i<sizeof($hs_protein); $i++){
-            $protein = $hs_protein[$i];
-            echo "<button class='food_choice'>$protein</button>";
-        }
-        ?>
-    </div>
-    <?php
-    } else {
-        echo '';
-    }
-    ?>
+                <?php
+                // Protein
+                if (sizeof($hs_protein) > 1) {
+                ?>
+                <div class="protein">
+                    <?php
+                    for($i = 0; $i<sizeof($hs_protein); $i++){
+                        $protein = $hs_protein[$i];
+                        echo "<button class='food_choice'>$protein</button>";
+                    }
+                    ?>
+                </div>
+                <?php
+                } else {
+                    echo '';
+                }
+                ?>
 
-    <?php
-    // More
-    if (sizeof($hs_more) > 1) {
-    ?>
-    <div class="more">
-        <?php
-        for($i = 0; $i<sizeof($hs_more); $i++){
-            $more = $hs_more[$i];
-            echo "<button class='food_choice'>$more</button>";
-        }
-        ?>
-    </div>
-    <?php
-    } else {
-        echo '';
-    }
-    ?>
+                <?php
+                // More
+                if (sizeof($hs_more) > 1) {
+                ?>
+                <div class="more">
+                    <?php
+                    for($i = 0; $i<sizeof($hs_more); $i++){
+                        $more = $hs_more[$i];
+                        echo "<button class='food_choice'>$more</button>";
+                    }
+                    ?>
+                </div>
+                <?php
+                } else {
+                    echo '';
+                }
+                ?>
 
-    <?php
-    // Toppings
-    if (sizeof($hs_toppings) > 1) {
-    ?>
-    <div class="toppings">
-        <?php
-        for($i = 0; $i<sizeof($hs_toppings); $i++){
-            $toppings = $hs_toppings[$i];
-            echo "<button class='food_choice'>$toppings</button>";
-        }
-        ?>
-    </div>
-    <?php
-    } else {
-        echo '';
-    }
-    ?>
+                <?php
+                // Toppings
+                if (sizeof($hs_toppings) > 1) {
+                ?>
+                <div class="toppings">
+                    <?php
+                    for($i = 0; $i<sizeof($hs_toppings); $i++){
+                        $toppings = $hs_toppings[$i];
+                        echo "<button class='food_choice'>$toppings</button>";
+                    }
+                    ?>
+                </div>
+                <?php
+                } else {
+                    echo '';
+                }
+                ?>
 
-    <?php
-    // Drinks
-    if (sizeof($hs_drinks) > 1) {
-    ?>
-    <div class="drinks">
-        <?php
-        for($i = 0; $i<sizeof($hs_drinks); $i++){
-            $drinks = $hs_drinks[$i];
-            echo "<button class='food_choice'>$drinks</button>";
-        }
-        ?>
-    </div>
-    <?php
-    } else {
-        echo '';
-    }
-    ?>
+                <?php
+                // Drinks
+                if (sizeof($hs_drinks) > 1) {
+                ?>
+                <div class="drinks">
+                    <?php
+                    for($i = 0; $i<sizeof($hs_drinks); $i++){
+                        $drinks = $hs_drinks[$i];
+                        echo "<button class='food_choice'>$drinks</button>";
+                    }
+                    ?>
+                </div>
+                <?php
+                } else {
+                    echo '';
+                }
+                ?>
 
-    <?php
-    // Sides
-    if (sizeof($hs_sides) > 1) {
-    ?>
-    <div class="sides">
-        <?php
-        for($i = 0; $i<sizeof($hs_sides); $i++){
-            $sides = $hs_sides[$i];
-            echo "<button class='food_choice'>$sides</button>";
-        }
-        ?>
-    </div>
-    <?php
-    } else {
-        echo '';
-    }
-    ?>
-</div>
+                <?php
+                // Sides
+                if (sizeof($hs_sides) > 1) {
+                ?>
+                <div class="sides">
+                    <?php
+                    for($i = 0; $i<sizeof($hs_sides); $i++){
+                        $sides = $hs_sides[$i];
+                        echo "<button class='food_choice'>$sides</button>";
+                    }
+                    ?>
+                </div>
+                <?php
+                } else {
+                    echo '';
+                }
+                ?>
+            </div>
+        </div>
+    
