f8a3a3bcbfa83b0b9cde99b388dc1b148310edbf
Kdolphin2284 <71986227+Kdolphin2284@users.noreply.github.com>
2022-02-08 22:21:04 -0500

    Large Update
    Structured 4/6 alpha webpages, file organization

---
+++ databaseInfoTest/includes/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "thedolr4_idm216";
+$dbPassword = "Gooch@228744";
+$dbName = "thedolr4_idm216";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+// if ($conn) {
+//     echo "Working";
+// } else {
+//     echo "Not working";
+// };
+
+?>
