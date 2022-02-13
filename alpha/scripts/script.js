c796516ac62d89c3cf1204d6bceb479836266bf2
Kdolphin2284 <71986227+Kdolphin2284@users.noreply.github.com>
2022-02-13 14:38:23 -0500

    Finished Alpha Edits
    Edited Alpha

--- alpha/scripts/script.js
+++ alpha/scripts/script.js
@@ -2,22 +2,58 @@ let hamburger = document.getElementById('header-menu');
 let mobileNav = document.getElementById('mobile-nav');
 let x = document.getElementById('x');
 
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
 hamburger.addEventListener("click", function(){
-    if (mobileNav.style.display == 'none') {
-        mobileNav.style.display = 'block';
-        console.log('mobile navigation open');
-    } else {
-        mobileNav.style.display = 'none';
-        console.log('mobile navigation closed');
+    if (!mobileNav.classList.contains('onScreen')) {
+        mobileNav.classList.add('onScreen');
+        console.log('Mobile navigation showing');
+        disableScroll();
     }
 })
 
 x.addEventListener("click", function(){
-    if (mobileNav.style.display == 'none') {
-        mobileNav.style.display = 'block';
-        console.log('mobile navigation open');
-    } else {
-        mobileNav.style.display = 'none';
-        console.log('mobile navigation closed');
+    if (mobileNav.classList.contains('onScreen')) {
+        mobileNav.classList.remove('onScreen');
+        console.log('Mobile navigation not showing');
+        enableScroll();
     }
 })
